1c4363136a4b78080744cf5acc781f4f58eb481d
evanaskanazi <54242948+evanaskanazi@users.noreply.github.com>
2019-08-23 15:44:00 +0300

    Update jsondata.py

--- jsondata.py
+++ jsondata.py
@@ -1,6 +1,7 @@
-#Script 
+#Script 1
 #For simply printing out part of read in scraped data, json reading and data slicing can work to print out specific components of the data after it's read in
-
+#data.txt is the desired json file to be read in
+import json
 with open('data.txt') as json_file:
     data = json.load(json_file)
     for p in data['{ "_id" :']:
@@ -12,3 +13,20 @@ with open('data.txt') as json_file:
         
 #slicing data by printing sections
 print(data[::2])    
+#Script 2
+#The json data is loaded using python functions for loading json data and then parsed according to desired output
+jsonparse = json.loads("data.txt")
+print(json.dumps(jasonparse, indent=4, sort_keys=True))
+#It can be iterated over the full json file
+for x in jsonparse:
+	print("%s: %d" % (x, loaded_json[x]))
+    
+#Desired sections of the json dictionary can be loaded into pytrhon and printed individually as follows, printing 
+#invididual sections of the data
+with open('data.txt', 'r') as f:
+    jsonparse = json.load(f)
+
+for x in jsonparse:
+    print(x['_id'])
+    print(x['timestamp'])
+    print(x[entry])
